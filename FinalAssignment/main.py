12a5f8ab8af122d773340ef01489772e8c2d37e5
Manuel Osvaldo Olguín <molguin@kth.se>
2017-11-16 19:46:59 -0500

    refactored cross-validation

--- FinalAssignment/main.py
+++ FinalAssignment/main.py
@@ -106,17 +106,19 @@ def shuffle_data(data_in, data_out):
 
 
 @time_func
-def k_fold_cross_validation(cf, data_in, data_out, k=10):
+def k_fold_cross_validation(cf, dataset, k=10):
     """
     Performs k_fold cross validation on a dataset given a specific classifier.
     :param cf: Classifier to use for the cross-validation
-    :param data_in: Input data.
-    :param data_out: Output data, labels.
+    :param dataset: Dataset tuple in format (train_in, train_out, test_in,
+    test_out)
     :param k: Optional k-fold parameter. Default is 10.
     :return: Tuple containing the average accuracy and standard deviation
     obtained through cross-validation.
     """
     assert k > 1
+    data_in = np.vstack((dataset[0], dataset[2]))
+    data_out = np.vstack((dataset[1], dataset[3]))
 
     # shuffle data, then partition
     data_in, data_out = shuffle_data(data_in, data_out)
@@ -143,66 +145,14 @@ def k_fold_cross_validation(cf, data_in, data_out, k=10):
 
 
 @time_func
-def cross_validation(k=10):
-    print('Performing {}-fold cross validation on models.'.format(k))
-    results = dict()
+def cross_validate(datasets):
+    classifier = LabelPowerset(LinearSVC())
+    results = map(lambda dset: k_fold_cross_validation(classifier,
+                                                       dset), datasets)
 
-    for prefix in coded_output_file_prefixes:
-        training_file = 'coded_output/' \
-                        '{}_new_training_data_step_2.0.txt'.format(prefix)
-        test_file = 'coded_output/' \
-                    '{}_new_test_data_step_2.0.txt'.format(prefix)
-
-        train_in, train_out = load_data(training_file)
-        test_in, test_out = load_data(test_file)
-
-        total_data_in = np.vstack((train_in, test_in))
-        train_in = test_in = None
-        gc.collect()
-
-        total_data_out = np.vstack((train_out.toarray(),
-                                    test_out.toarray()))
-        train_out = test_out = None
-        gc.collect()
-
-        print('Beginning cross-validation for dataset {}...'.format(prefix))
-        classifier = LabelPowerset(LinearSVC())
-
-        # print('Custom cross-validation...')
-        mean_k_fold, std_k_fold = k_fold_cross_validation(classifier,
-                                                          total_data_in,
-                                                          total_data_out,
-                                                          k=k)
-
-        # print('Done.\nBuilt in cross-validation...')
-        # scores_built_in = time_func(cross_val_score)(classifier,
-        #                                             total_data_in,
-        #                                             total_data_out,
-        #                                             cv=k)
-
-        print('Done')
-        total_data_out = total_data_in = None
-        gc.collect()
-
-        results[prefix] = (mean_k_fold, std_k_fold)
-        # results[prefix] = (scores_built_in.mean(), scores_built_in.std(),
-        #                   mean_k_fold, std_k_fold)
-        # print('{}: {}'.format(prefix, results[prefix]))
-
-    print('''
-
---------- * ---------
-
-Accuracy ratings per experiment (mean of {}-fold cross validation):
-
-        '''.format(k))
-
-    for prefix, stats in results.items():
-        print('{}: {} (std: {})'.format(prefix, stats[0], stats[1]))
-        # for prefix, stats in results.items():
-        #     print('{} -> built-in: {} (std: {}) | custom: {} (std: {
-        # })'.format(
-        #         prefix, stats[0], stats[1], stats[2], stats[3]))
+    print('Cross-validation results:')
+    for name, stats in zip(coded_output_file_prefixes, results):
+        print('{}: {} (std: {})'.format(name, stats[0], stats[1]))
 
 
 # @time_func
@@ -243,6 +193,6 @@ def parallel_learn_and_predict(datasets):
 
 
 if __name__ == '__main__':
-    # cross_validation()
     datasets = map(load_dataset, coded_output_file_prefixes)
+    cross_validate(datasets)
     parallel_learn_and_predict(datasets)
