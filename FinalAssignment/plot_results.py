9e34920cd397f8a1074014bda73735bffd8591da
Manuel Osvaldo Olguín <molguin@kth.se>
2017-11-21 11:49:17 -0500

    report

--- FinalAssignment/plot_results.py
+++ FinalAssignment/plot_results.py
@@ -1,27 +1,34 @@
 import numpy as np
+import time
 from matplotlib import pyplot as plt
 
 
 def main():
-    data = np.loadtxt('results/profiling_data.txt',
-                      dtype=np.float,
-                      delimiter=',')
-    cpu = data[:, :-1]
-    ram = data[:, -1]
+    data = np.genfromtxt('results/profiling_data.txt',
+                         dtype=np.float,
+                         delimiter=',',
+                         names=True)
 
     f, axes = plt.subplots(3, 3, sharex='col', sharey='row')
 
     for i in range(9):
         x = int(i / 3)
         y = i % 3
-        axes[x, y].plot(cpu[:, i])
 
-    plt.ylabel('CPU%')
-    plt.xlabel('Time [s]')
+        if i == 4:
+            axes[x, y].plot(data['cpu_total'])
+            axes[x, y].set_ylabel('CPU%')
+            axes[x, y].set_xlabel('Time [s]')
+            axes[x, y].set_title('cpu_total')
+        else:
+            index = 'cpu' + str(i) if i < 4 else 'cpu' + str(i - 1)
+            axes[x, y].plot(data[index])
+            axes[x, y].set_title(index)
+
     plt.show()
 
     # plt.plot(ram)
-    ram = ram / np.power(10, 9)
+    ram = data['ram'] / np.power(10, 9)
     plt.fill_between(range(len(ram)), ram, color='orange')
     plt.ylabel('RAM [GB]')
     plt.xlabel('Time [s]')
